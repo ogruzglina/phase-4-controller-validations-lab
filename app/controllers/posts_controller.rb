6aed5b86a07d2d47ff8136ebad849a0368308587
Olha Gruzglina <olga.zhmura@gmail.com>
2022-03-16 09:50:44 -0400

    commit tree f520ab5302fae710df08bbdff8b1f443e42a0582

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,28 @@
 class PostsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def show
-    post = Post.find(params[:id])
-    
+    post = find_post
     render json: post
   end
 
   def update
-    post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post = find_post
+    post.update!(post_params)
     render json: post
   end
 
   private
-
   def post_params
     params.permit(:category, :content, :title)
+  end
+
+  def find_post
+    Post.find(params[:id])
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
 end
